641e996ddea2aaf18a379dcba496d4f8f7c02d60
Francisco Carmona Suarez <fcsuarez96@gmail.com>
2019-09-25 18:11:02 +0200

    Uploading exercises

---
+++ Exercises/Unit 4_Conditionals/Exercise06.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercise06 {
+	
+	public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter the height (m) in order to calculate the fall time");
+    final double g = 9.81;
+    double height = sc.nextDouble();
+    
+    double result = Math.sqrt(2*height/g);
+    
+    System.out.printf("Fall time: %.2f seconds",result);
+    
+	}
+}
+
