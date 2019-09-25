c8fab288bc3ab5492e6a08f4be92262ab12671d2
Francisco Carmona Suarez <fcsuarez96@gmail.com>
2019-09-25 17:12:04 +0200

    Updating Exercises

--- Exercises/Unit 4_Conditionals/Exercise01.java
+++ Exercises/Unit 4_Conditionals/Exercise01.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Exercise01 {
 	
-	public static void main (String[] args) {
+  public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
     
     System.out.println("Enter a day of the week to get your first subject of the day");
@@ -40,6 +40,6 @@ public class Exercise01 {
       
     }
     
-	}
+  }
 }
 
