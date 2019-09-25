5f3244a96ea8c7ccc7f3c73b419f8131e39e1c76
Francisco Carmona Suarez <fcsuarez96@gmail.com>
2019-09-25 17:16:37 +0200

    Updating Exercises

--- Exercises/Unit 3_Input/Exercise01.java
+++ Exercises/Unit 3_Input/Exercise01.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Exercise01 {
 
-	public static void main (String[] args) {
+  public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter a number");
     
