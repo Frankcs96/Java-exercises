3060b5a25cba57a0c9b9c2551a60e34670ee8b06
Francisco Carmona Suarez <fcsuarez96@gmail.com>
2019-09-26 11:50:50 +0200

    Uploading exercises

--- Exercises/Unit 4_Conditionals/Exercise08.java
+++ Exercises/Unit 4_Conditionals/Exercise08.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Exercise08 {
-	
+
   public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("***Calculating average of 3 grades***");
---
+++ Exercises/Unit 4_Conditionals/Exercise09.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Exercise09 {
+
+  public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("***Second grade equation solver (ax^2 + bx + c = 0)***");
+    System.out.println("Enter value of a");
+    double a = sc.nextDouble();
+    System.out.println("Enter value of b");
+    double b = sc.nextDouble();
+    System.out.println("Enter value of c");
+    double c = sc.nextDouble();
+    
+    if ((a == 0) && (b == 0) && (c != 0)) {
+      System.out.println("No solution.");
+    }
+    
+    else if ((a == 0) && (b == 0) && (c == 0)) {
+      System.out.println("Infinite solutions.");
+    }
+    
+    else if ((a == 0) && (b != 0) && (c != 0)) {
+      System.out.println(" only 1 solution x = " + (-c / b));
+    }
+    
+    
+    else {
+        
+      double discriminant = Math.pow(b,2) - (4 * a * c);
+      
+      if (discriminant < 0) {
+          System.out.println("Equation has no real solutions");
+      }  else {
+        System.out.println("x1 = " + (-b + Math.sqrt(discriminant))/(2 * a));
+        System.out.println("x2 = " + (-b - Math.sqrt(discriminant))/(2 * a));
+      }
+    }
+  }
+}
+
