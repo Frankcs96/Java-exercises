6ec35f67659e6310d1b9f7e0cdd9f3a84de6c416
Francisco Carmona Suarez <fcsuarez96@gmail.com>
2019-09-28 13:27:22 +0200

    Exercise 20 completed

--- Exercises/Unit 4_Conditionals/Exercise20.java
+++ Exercises/Unit 4_Conditionals/Exercise20.java
@@ -4,6 +4,9 @@ public class Exercise20 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     
+    System.out.println("****Palindrome checker****");
+    System.out.println("Enter a number (max 5 digits)");
+  
     int number = sc.nextInt();
     boolean isPalindromic = false;
     
@@ -25,10 +28,31 @@ public class Exercise20 {
     }
     
     if ((number >= 1000) && (number <= 9999)) {
-      if ((number % 10) == (number / 1000) && ) {
+      if ((number % 10) == (number / 1000) && (number % 100 / 10) == (number % 1000 / 100)) {
         isPalindromic = true; 
       }
     }
+    
+    
+    if ((number >= 10000) && (number <= 99999)) {
+      if ((number % 10) == (number / 10000) && (number % 100 / 10) == (number % 10000 / 1000)) {
+        isPalindromic = true;
+      }
+      
+    }
+    
+    if (number >= 1000000) {
+      System.out.println("Number too long remember to enter a maximum of 5 digits");
+    } else {
+      
+      if (isPalindromic) {
+        System.out.println("The number: " + number + " is a palindrome");
+      } else {
+        System.out.println("The number: " + number + " is not a palindrome");
+      }
+    }
+    
+    
 
     
   }
