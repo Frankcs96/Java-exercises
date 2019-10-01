78de7b77ea9689de4f2819ae2b18c3b384565ff1
Francisco Carmona Suárez <fcsuarez96@gmail.com>
2019-10-01 15:25:03 +0200

    Uploading Exercises

---
+++ Exercises/Unit 5 Loops/Exercise05.java
@@ -0,0 +1,11 @@
+
+public class Exercise05 {
+  public static void main(String[] args) {
+    int number = 320;
+    
+    while (number >= 160) {
+      System.out.println(number);
+      number-=20;
+    }
+  }
+}
