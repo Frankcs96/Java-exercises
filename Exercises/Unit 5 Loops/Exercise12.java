92b4d01222cafc951e75a95560464ea76067fa27
Francisco Carmona Suarez <fcsuarez96@gmail.com>
2019-10-03 06:07:22 +0200

    Uploading exercises

---
+++ Exercises/Unit 5 Loops/Exercise12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise12 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How many numbers of the fibonacci sequence do you want?");
+    int number = sc.nextInt();
+    int lastNum1 = 0;
+    int lastNum2= 0;
+    
+    
+    for (int i = 0; i < number; i++) {
+      if (i == 0) {
+        System.out.println(i);
+        lastNum1 = i;
+      } else if (i == 1) {
+        System.out.println(i);
+        lastNum2 = i;
+      } else {
+        int sum = lastNum1 + lastNum2;
+        System.out.println(sum);
+        lastNum1 = lastNum2;
+        lastNum2 = sum;
+               
+      }
+    }
+  }
+}
