4af63e3b25961925ef5c4bbff0a9007e4dd3305e
Francisco Carmona Suarez <fcsuarez96@gmail.com>
2019-10-08 06:25:42 +0200

    Uploading exercises

--- Exercises/Unit 5 Loops/Exercise31.java
+++ Exercises/Unit 5 Loops/Exercise31.java
@@ -7,21 +7,16 @@ public class Exercise31 {
     System.out.print("Enter the height of the L: ");
     int height = sc.nextInt();
     
-    for (int i = 1; i <= height; i++) {
+    for (int i = 1; i < height; i++) {
       
-      for (int j = 0; j < 1; j++) {
-        System.out.print("*");
-               
-      }
-      
-      for (int k = 1; k < height / 2 + 1; k++) {
-        if (i == height) {
-          System.out.print(" *");
-          
-        }
-      }      
-      System.out.println();
+      System.out.println("*");
     }
+      
+    for (int k = 1; k <= height / 2 + 1; k++) {
+      System.out.print("* ");    
+    }      
+      
+    
       
        
   }
