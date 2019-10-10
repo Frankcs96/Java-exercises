14632a8adf77d17a24ccb5f43be4688364ca0403
Francisco Carmona Suárez <fcsuarez96@gmail.com>
2019-10-10 10:09:51 +0200

    Finishing ex 40

--- Exercises/Unit 5 Loops/Exercise40.java
+++ Exercises/Unit 5 Loops/Exercise40.java
@@ -16,7 +16,53 @@ public class Exercise40 {
     } while (height < 3 || height % 2 == 0);
     
     
+    for (int i = 0; i < height / 2 + 1; i++) {
+      
+      for (int j = height / 2; j > i ; j--) {
+        System.out.print(" ");
+      }
+      
+      for (int j = 0; j <= i*2; j++) {
+        
+        if (i + j == 0 || i + j == i || j-i == i) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+        
+        
+      }
+      
+      
+      
+      System.out.println();
+    }
     
+    for (int i = 0; i < height / 2; i++) { // 0 1 2
+      
+      for (int j = 0; j <= i; j++) {
+        System.out.print(" ");
+      }
+      
+      for (int j = height - 2; j > i*2 ; j--) {
+        if ( j - i == 1 + i || i + j-i == height - 2) { 
+          
+          System.out.print("*");
+          
+          
+        } else {
+          System.out.print(" ");
+          
+          
+        }
+        
+      }
+      
+      
+      
+      
+      System.out.println();
+    }
     
   }
   
