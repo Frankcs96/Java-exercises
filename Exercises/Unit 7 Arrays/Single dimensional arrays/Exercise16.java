17106363ae8fa6bf8095f737a0ad6761ea5084ac
Frankcs96 <fcsuarez96@gmail.com>
2019-10-31 20:34:26 +0100

    Uploading Exercises

---
+++ Exercises/Unit 7 Arrays/Single dimensional arrays/Exercise16.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Exercise16 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int[] numbers = new int[20];
+
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = (int) (Math.random() * ((400 - 0) + 1)) + 0;
+
+    }
+
+    for (int i : numbers) {
+      System.out.print(i + " ");
+    }
+
+    System.out
+        .print("\nWhich numbers do you want to highlight? ( 1-Multiples of 5, 2-multiples of 7): ");
+
+    int option = sc.nextInt();
+
+    if (option == 1) {
+      for (int i : numbers) {
+        if (i % 5 == 0) {
+          System.out.print("[" + i + "]" + " ");
+        } else {
+          System.out.print(i + " ");
+        }
+      }
+    }
+
+    if (option == 2) {
+      for (int i : numbers) {
+        if (i % 7 == 0) {
+          System.out.print("[" + i + "]" + " ");
+        } else {
+          System.out.print(i + " ");
+        }
+      }
+    }
+    
+    if (option != 1 && option != 2) {
+      System.out.println("Wrong input try again");
+    }
+  }
+
+}
