269d43e8fc8e9cf5e5c6c57e2f7fe5ac6039d655
Frankcs96 <fcsuarez96@gmail.com>
2019-11-10 17:31:33 +0100

    Uploading Exercises

--- Exercises/Unit 8 Functions/Exercise16.java
+++ Exercises/Unit 8 Functions/Exercise16.java
@@ -1,7 +1,7 @@
 
 public class Exercise16 {
   public static void main(String[] args) {
-    for (int i = 1; i < 99999; i++) {
+    for (int i = 1; i <= 99999; i++) {
       if (maths.Main.isPalindrome(i)) {
         System.out.print(i + " ");
       }
