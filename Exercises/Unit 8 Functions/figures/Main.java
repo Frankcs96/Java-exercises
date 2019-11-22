4d0b1a9687dee44def46f87a7aef61de60f98cba
Frankcs96 <fcsuarez96@gmail.com>
2019-11-22 18:52:14 +0100

    Uploading exercises

--- Exercises/Unit 8 Functions/figures/Main.java
+++ Exercises/Unit 8 Functions/figures/Main.java
@@ -1,7 +1,7 @@
 package figures;
 
 public class Main {
-
+  //Draw a line with the character that you want N times.
   public static String drawLine(char charac, int size) {
     String line = "";
     for (int i = 0; i < size; i++) {
